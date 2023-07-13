5a44d057ac1e050fb8528e6457d9e6e0038be360
WojLip <131802481+WojLip@users.noreply.github.com>
2023-07-13 21:35:26 +0200

    Add files via upload

---
+++ program/constants (1).h
@@ -0,0 +1,13 @@
+#define hipLOffset 35
+#define kneeLOffset 80
+#define ankleLOffset 80
+
+#define hipROffset 120
+#define kneeROffset 60
+#define ankleROffset 85
+
+#define l1 5
+#define l2 5.7
+
+#define stepClearance 1
+#define stepHeight 10
---
+++ program/constants.h
@@ -0,0 +1,12 @@
+#define hipLOffset 35
+#define kneeLOffset 180
+#define ankleLOffset 80
+#define hipROffset 120
+#define kneeROffset 60
+#define ankleROffset 85
+
+#define l1 3.8
+#define l2 3
+
+#define stepClearance 1
+#define stepHeight 10
